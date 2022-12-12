c0807b65ba8b5c91cdb753ee20ab0d13dca92cf0
certihimo <kalidahm3d@gmail.com>
2022-12-12 14:48:59 +0300

    complete

--- main.js
+++ main.js
@@ -3,7 +3,35 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const errMsg = document.getElementById('modal') 
+errMsg.classList.add('hidden') 
 
+
+
+const liker = (e) => { 
+
+  mimicServerCall()  
+  .then(()=>{  
+    if(e.target.textContent == EMPTY_HEART){  
+      e.target.textContent = FULL_HEART       
+      e.target.classList.add('activated-heart') 
+    }
+    else{
+      e.target.textContent = EMPTY_HEART      
+      e.target.classList.remove('activated-heart') 
+    }
+  })
+  .catch(()=>{  
+    errMsg.classList.remove('hidden')  
+    setTimeout(function(){  
+      errMsg.classList.add('hidden')
+    }, 3000)
+  })
+}
+let hearts = document.getElementsByClassName('like-glyph') 
+for (let heart of hearts) { 
+  heart.addEventListener('click', liker) 
+}
 
 
 
